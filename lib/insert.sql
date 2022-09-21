0f947aa7b6ee2f21b9da6a2f03dfad8b51604ff0
MonsterBasket <blazza1@hotmail.com>
2022-09-21 16:26:46 +1000

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (Name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 5, 'M', 'Brown', 'Angry', 1),
+('Rowdy', 3, 'M', 'Black', 'Hungry', 1),
+('Tabitha', 7, 'F', 'White', 'Angry', 1),
+('Sergeant Brown', 16, 'M', 'Brown', 'Hungry', 1),
+('Melissa', 2, 'F', 'Brown', 'Angry', 1),
+('Grinch', 5, 'M', 'Black', 'Hungry', 1),
+('Wendy', 4, 'F', 'White', 'Lovable', 1),
+(NULL, 7, 'M', 'Brown', 'Vicious', 1);
